692b0eaa138e733ebb30a6aa23d4ee2f2ab3d3a8
Danf96 <danielthomasf@gmail.com>
2023-12-23 16:43:44 -0800

    Add textured walls and floors

--- src/main.c
+++ src/main.c
@@ -1,39 +1,50 @@
 #include <math.h>
+#include <stdint.h>
+
 #include "raylib.h"
 #include "raymath.h"
 // Following raycasting tutorial from https://lodev.org/cgtutor/raycasting.html
+// https://github.com/raylib-extras/examples-c/tree/main/write_to_image
+// can update the texture with UpdateTexture
+#define map_width 24
+#define map_height 24
 
-#define mapWidth 24
-#define mapHeight 24
+#define screen_width 640
+#define screen_height 480
 
-void draw_walls(Vector2 pos, Vector2 dir, Vector2 cam_plane);
+#define floor_tex_num 3
+#define ceil_tex_num 6
 
-int world_map[mapWidth][mapHeight] =
-    {
-        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2, 0, 0, 0, 0, 3, 0, 3, 0, 3, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 4, 0, 0, 0, 0, 5, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 4, 0, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+void draw_floor_and_ceil(Vector2 pos, Vector2 dir, Vector2 cam_plane, Image tex[8], Image *image_buffer);
+void draw_walls(Vector2 pos, Vector2 dir, Vector2 cam_plane, Image tex[8], Image *image_buffer);
+
+int world_map[map_width][map_height] =
+{
+  {8,8,8,8,8,8,8,8,8,8,8,4,4,6,4,4,6,4,6,4,4,4,6,4},
+  {8,0,0,0,0,0,0,0,0,0,8,4,0,0,0,0,0,0,0,0,0,0,0,4},
+  {8,0,3,3,0,0,0,0,0,8,8,4,0,0,0,0,0,0,0,0,0,0,0,6},
+  {8,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,6},
+  {8,0,3,3,0,0,0,0,0,8,8,4,0,0,0,0,0,0,0,0,0,0,0,4},
+  {8,0,0,0,0,0,0,0,0,0,8,4,0,0,0,0,0,6,6,6,0,6,4,6},
+  {8,8,8,8,0,8,8,8,8,8,8,4,4,4,4,4,4,6,0,0,0,0,0,6},
+  {7,7,7,7,0,7,7,7,7,0,8,0,8,0,8,0,8,4,0,4,0,6,0,6},
+  {7,7,0,0,0,0,0,0,7,8,0,8,0,8,0,8,8,6,0,0,0,0,0,6},
+  {7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,6,0,0,0,0,0,4},
+  {7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,6,0,6,0,6,0,6},
+  {7,7,0,0,0,0,0,0,7,8,0,8,0,8,0,8,8,6,4,6,0,6,6,6},
+  {7,7,7,7,0,7,7,7,7,8,8,4,0,6,8,4,8,3,3,3,0,3,3,3},
+  {2,2,2,2,0,2,2,2,2,4,6,4,0,0,6,0,6,3,0,0,0,0,0,3},
+  {2,2,0,0,0,0,0,2,2,4,0,0,0,0,0,0,4,3,0,0,0,0,0,3},
+  {2,0,0,0,0,0,0,0,2,4,0,0,0,0,0,0,4,3,0,0,0,0,0,3},
+  {1,0,0,0,0,0,0,0,1,4,4,4,4,4,6,0,6,3,3,0,0,0,3,3},
+  {2,0,0,0,0,0,0,0,2,2,2,1,2,2,2,6,6,0,0,5,0,5,0,5},
+  {2,2,0,0,0,0,0,2,2,2,0,0,0,2,2,0,5,0,5,0,0,0,5,5},
+  {2,0,0,0,0,0,0,0,2,0,0,0,0,0,2,5,0,5,0,5,0,5,0,5},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5},
+  {2,0,0,0,0,0,0,0,2,0,0,0,0,0,2,5,0,5,0,5,0,5,0,5},
+  {2,2,0,0,0,0,0,2,2,2,0,0,0,2,2,0,5,0,5,0,0,0,5,5},
+  {2,2,2,2,1,2,2,2,2,2,2,1,2,2,2,5,5,5,5,5,5,5,5,5}
+};
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -42,13 +53,30 @@ int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
+    InitWindow(screen_width, screen_height, "raylib raycaster");
+    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+    
     Vector2 pos = (Vector2){.x = 22.f, .y = 12.f};
     Vector2 dir = (Vector2){.x = -1.f, .y = 0.f};
     Vector2 cam_plane = (Vector2){.x = 0.f, .y = 0.66f};
+    
+    Image tex[8] = {0};
+    // "../../" needed because of default windows build path for quick testing
+    tex[0] = LoadImage("../../res/tex/eagle.png");
+    tex[1] = LoadImage("../../res/tex/redbrick.png");
+    tex[2] = LoadImage("../../res/tex/purplestone.png");
+    tex[3] = LoadImage("../../res/tex/greystone.png");
+    tex[4] = LoadImage("../../res/tex/bluestone.png");
+    tex[5] = LoadImage("../../res/tex/mossy.png");
+    tex[6] = LoadImage("../../res/tex/wood.png");
+    tex[7] = LoadImage("../../res/tex/colorstone.png");
 
-    InitWindow(640, 480, "raylib raycaster");
+    Image image_buffer = GenImageColor(GetScreenWidth(), GetScreenHeight(), BLACK);
+    Texture display_texture = LoadTextureFromImage(image_buffer);
 
-    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+    // Image clear_buffer = GenImageColor(GetScreenWidth(), GetScreenHeight(), GRAY);
+
+    
     //--------------------------------------------------------------------------------------
 
     // Main game loop
@@ -87,14 +115,20 @@ int main(void)
             dir = Vector2Rotate(dir, rot_speed);
             cam_plane = Vector2Rotate(cam_plane, rot_speed);
         }
+        draw_floor_and_ceil(pos, dir, cam_plane, tex, &image_buffer);
+        draw_walls(pos, dir, cam_plane, tex, &image_buffer);
+        UpdateTexture(display_texture, image_buffer.data);
+
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-        ClearBackground(BLACK);
-        draw_walls(pos, dir, cam_plane);
+        DrawTexture(display_texture, 0, 0, RAYWHITE);
 
         EndDrawing();
+        // not clearing the buffer because we're always writing over every pixel
+        // ImageDraw(&image_buffer, clear_buffer, (Rectangle){.x = 0, .y = 0, .height = screen_height, .width = screen_width}, (Rectangle){.x = 0, .y = 0, .height = screen_height, .width = screen_width}, RAYWHITE);
+
         //----------------------------------------------------------------------------------
     }
 
@@ -105,13 +139,57 @@ int main(void)
 
     return 0;
 }
-
-void draw_walls(Vector2 pos, Vector2 dir, Vector2 cam_plane)
+void draw_floor_and_ceil(Vector2 pos, Vector2 dir, Vector2 cam_plane, Image tex[8], Image *image_buffer)
 {
-    for (int x = 0; x < GetScreenWidth(); x++)
+    for (int y = 0; y < screen_height; y++)
+    {
+        // direction vectors for leftmost and rightmost rays
+        Vector2 ray_left = (Vector2){.x = dir.x - cam_plane.x, .y = dir.y - cam_plane.y};
+        Vector2 ray_right = (Vector2){.x = dir.x + cam_plane.x, .y = dir.y + cam_plane.y};
+
+        // y pos compared to horizon
+        int p = y - screen_height / 2;
+
+        // horizontal distance from camera to floor
+        float pos_z = 0.5 * screen_height;
+
+        // distance from camera to floor for current row
+        float row_dist = pos_z / p;
+
+        Vector2 floor_step = (Vector2){.x = row_dist * (ray_right.x - ray_left.x) / screen_width, .y = row_dist * (ray_right.y - ray_left.y) / screen_width};
+
+        Vector2 floor = (Vector2){.x = pos.x + row_dist * ray_left.x, .y = pos.y + row_dist * ray_left.y};
+
+        for (int x = 0; x < screen_width; x++)
+        {
+            int cell_x = (int)floor.x;
+            int cell_y = (int)floor.y;
+
+            int t_x = (int)(tex[floor_tex_num].width * (floor.x - cell_x)) & (tex[floor_tex_num].width - 1);
+            int t_y = (int)(tex[floor_tex_num].height * (floor.y - cell_y)) & (tex[floor_tex_num].height - 1);
+
+            floor = Vector2Add(floor, floor_step);
+            // floor
+            Color color = GetImageColor(tex[floor_tex_num], t_x, t_y);
+            color = (Color){.a = color.a, .r = (color.r >> 1) & 8355711, .g = (color.g >> 1) & 8355711, .b = (color.b >> 1) & 8355711};
+            ImageDrawPixel(image_buffer, x, y, color);
+
+            // ceiling
+            color = GetImageColor(tex[ceil_tex_num], t_x, t_y);
+            color = (Color){.a = color.a, .r = (color.r >> 1) & 8355711, .g = (color.g >> 1) & 8355711, .b = (color.b >> 1) & 8355711};
+            ImageDrawPixel(image_buffer, x, screen_height - y - 1, color);
+        }
+
+    }
+    
+}
+
+void draw_walls(Vector2 pos, Vector2 dir, Vector2 cam_plane, Image tex[8], Image *image_buffer)
+{
+    for (int x = 0; x < screen_width; x++)
     {
         // calc ray pos and direction (switch to raylib structs)
-        float camera_x = 2 * x / (float)GetScreenWidth() - 1;
+        float camera_x = 2 * x / (float)screen_width - 1;
         Vector2 ray_dir = (Vector2){.x = dir.x + cam_plane.x * camera_x, .y = dir.y + cam_plane.y * camera_x};
 
         // which cell we're in
@@ -175,34 +253,32 @@ void draw_walls(Vector2 pos, Vector2 dir, Vector2 cam_plane)
         else
             perp_wall_dist = (side_dist.y - delta_dist.y);
 
-        int line_height = (int)(GetScreenHeight() / perp_wall_dist);
-        int draw_start = -line_height / 2 + GetScreenHeight() / 2;
-        if (draw_start < 0)
-            draw_start = 0;
+        int line_height = (int)(screen_height / perp_wall_dist);
+        int draw_start = -line_height / 2 + screen_height / 2;
+        if (draw_start < 0) draw_start = 0;
+        int draw_end = line_height / 2 + screen_height / 2;
+        if (draw_end >= screen_height) draw_end = screen_height - 1;
 
-        Color color;
-        switch (world_map[map_x][map_y])
-        {
-        case 1:
-            color = RED;
-            break;
-        case 2:
-            color = GREEN;
-            break;
-        case 3:
-            color = BLUE;
-            break;
-        case 4:
-            color = WHITE;
-            break;
-        default:
-            color = YELLOW;
-        }
+        int tex_num = world_map[map_x][map_y] - 1;
 
-        if (side == 1)
+        float wall_x;
+        if (side == 0) wall_x = pos.y + perp_wall_dist * ray_dir.y;
+        else wall_x = pos.x + perp_wall_dist * ray_dir.x;
+        wall_x -= (float)floor(wall_x);
+
+        int tex_x = (int)(wall_x * (float)tex[tex_num].width);
+        if (side == 0 && ray_dir.x > 0) tex_x = tex[tex_num].width - tex_x - 1;
+        if (side == 1 && ray_dir.y < 0) tex_x = tex[tex_num].width - tex_x - 1;
+
+        float step = 1.0f * tex[tex_num].height / line_height;
+        float tex_pos = (draw_start - screen_height / 2 + line_height / 2) * step;
+        for (int y = draw_start; y < draw_end; y++)
         {
-            color = (Color){.a = color.a, .r = color.r / 2, .g = color.g / 2, .b = color.b / 2};
+            int tex_y = (int)tex_pos & (tex[tex_num].height - 1);
+            tex_pos += step;
+            Color color = GetImageColor(tex[tex_num], tex_x, tex_y); 
+            if(side == 1) color = (Color){.a = color.a, .r = (color.r >> 1) & 8355711, .g = (color.g >> 1) & 8355711, .b = (color.b >> 1) & 8355711};
+            ImageDrawPixel(image_buffer, x, y, color);
         }
-        DrawRectangle(x, draw_start, 1, line_height, color);
     }
 }
